44671a4457abcf7958a8a65d8e1b06e53003ddac
Bruno Lagoa <miroldols@gmail.com>
2018-11-28 15:43:01 -0200

    Projeto feito em reactjs

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Provider } from "react-redux";
+import { Route, BrowserRouter as Router } from "react-router-dom";
+import { Container } from "semantic-ui-react";
+
+import store from "./redux";
+
+import Home from "./screens/Home";
+import Admin from "./screens/Admin/index";
+import Restrito from "./screens/Restrito/index";
+import Login from "./screens/Login";
+import CreateAccount from "./CreateAccount";
 
 class App extends Component {
+  /*async componentDidMount() {
+    
+  }*/
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Provider store={store}>
+        <Router>
+          <Container>
+            <Route exact path="/" component={Home} />
+            <Route path="/admin" component={Admin} />
+            <Route path="/restrito" component={Restrito} />
+            <Route path="/login" component={Login} />
+            <Route path="/create-account" component={CreateAccount} />
+          </Container>
+        </Router>
+      </Provider>
     );
   }
 }
